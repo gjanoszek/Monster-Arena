fc9e6d83a7417642b932201aba5773870fcc546f
Wojtek Janoszek <wetcouch46wojtek@gmail.com>
2015-01-16 21:44:20 +0100

    Shared console

--- MonsterBattle.js
+++ MonsterBattle.js
@@ -22,8 +22,9 @@ var Monster = function (opts) {
             critMult: parseFloat($("#criticMultiplier" + monsterNum).val())
         };
     },
-    log1,
-    log2,
+    $log,
+    $battleStatusPlayer1,
+    $battleStatusPlayer2,
     uderzenie,
     isCritic,
     isAccurate;
@@ -69,44 +70,74 @@ Monster.prototype.uderz = function (monsterNum) {
 
 };
 
-var raport = function (player, daneUderzenia, monsterNum) {
-    var currentLogVal = window['log' + monsterNum].val();
-    if (daneUderzenia.isCritic) {
-        newLogVal = currentLogVal + "\n" + (player.przedstawSie() + ' deals critic ' + daneUderzenia.uderzenie + ' damage.');
-    } else if (daneUderzenia.uderzenie === 0) {
-        newLogVal = currentLogVal + "\n" + (player.przedstawSie() + ' misses!')
+var raport = function(monster1FightData, monster2FightData, round) {
+    var $log = $('#shared_battle_log'),
+        template = ['<li class="list-group-item">', '<span class="badge player1">14</span>', '<span class="badge player2">14</span>Round <span class="roundNo">1</span>', '</li>'].join(''),
+        $template = $(template);
+
+
+
+    $('.roundNo', $template).text(round);
+
+    if (monster1FightData.daneUderzenia.isCritic) {
+        $('.player1', $template).addClass('critic').text(monster1FightData.daneUderzenia.uderzenie);
+    } else if (monster1FightData.daneUderzenia.uderzenie === 0) {
+        $('.player1', $template).addClass('miss').text(0);
     } else {
-        newLogVal = currentLogVal + "\n" + (player.przedstawSie() + ' hits with ' + daneUderzenia.uderzenie + ' damage.');
+        $('.player1', $template).text(monster1FightData.daneUderzenia.uderzenie);
     }
-    window['log' + monsterNum].val(newLogVal);
+
+    if (monster2FightData.daneUderzenia.isCritic) {
+        $('.player2', $template).addClass('critic').text(monster2FightData.daneUderzenia.uderzenie);
+    } else if (monster2FightData.daneUderzenia.uderzenie === 0) {
+        $('.player2', $template).addClass('miss').text(0);
+    } else {
+        $('.player2', $template).text(monster2FightData.daneUderzenia.uderzenie);
+    }
+
+    $log.append($template);
+
+
 };
 
 var fight = function () {
     var winner,
         m1DaneUderzenia,
-        m2DaneUderzenia;
+        m2DaneUderzenia,
+        round = 0;
 
     // walka:
     while (monster1.hp > 0 && monster2.hp > 0) {
+        round += 1;
         m2DaneUderzenia = monster2.uderz(2);
         m1DaneUderzenia = monster1.uderz(1);
 
         monster1.hp -= m2DaneUderzenia.uderzenie;
-        raport(monster2, m2DaneUderzenia, 2);
+        //raport(monster2, m2DaneUderzenia, 2);
 
         monster2.hp -= m1DaneUderzenia.uderzenie;
-        raport(monster1, m1DaneUderzenia, 1);
+        //raport(monster1, m1DaneUderzenia, 1);
+
+        raport({
+            player: monster2,
+            daneUderzenia: m2DaneUderzenia
+        }, {
+            player: monster1,
+            daneUderzenia: m1DaneUderzenia
+        }, round);
+
     }
 
     // analiza walki:
     if (monster1.hp <= 0 && monster2.hp <= 0) {
-        alert("Everyone's dead...end of game!");
+        $battleStatusPlayer1.text('DEAD');
+        $battleStatusPlayer2.text('DEAD');
     } else if (monster1.hp > monster2.hp) {
         winner = monster1;
-        alert('The winner is...' + winner.przedstawSie() + '!');
+        $battleStatusPlayer1.text('WINNER');
     } else if (monster2.hp > monster1.hp) {
         winner = monster2;
-        alert('The winner is...' + winner.przedstawSie() + '!');
+        $battleStatusPlayer2.text('WINNER');
     }
 
 };
@@ -122,15 +153,18 @@ $(document).ready(function ($) {
             ['Troll', 90, 5, 4, 0.6, 7]
         ];
 
-    log1 = $("#log1");
-    log2 = $("#log2");
+    $log = $('#shared_battle_log');
+    $battleStatusPlayer1 = $('#battleStatusPlayer1');
+    $battleStatusPlayer2 = $('#battleStatusPlayer2');
 
 
     beginBattle.click(function () {
         var monster1opts = getMonsterInitData(1),
             monster2opts = getMonsterInitData(2);
 
-        $('.console').val('');
+        $log.empty();
+        $battleStatusPlayer1.empty();
+        $battleStatusPlayer2.empty();
 
         monster1 = new Monster(monster1opts);
         monster2 = new Monster(monster2opts);
