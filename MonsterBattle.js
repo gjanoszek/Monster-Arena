278d22bbe0d1e999194acf376082c4029e9f9b23
Grzesiek <grzechu.noname@gmail.com>
2020-03-17 22:30:48 +0100

    input update + round display

--- MonsterBattle.js
+++ MonsterBattle.js
@@ -42,13 +42,13 @@ class Monster {
             }
         }
         targetMonster.hp = targetMonster.hp - dmg;
-
-
+        return dmg;
     }
 }
 
 class Game {
     constructor() {
+        // this.monsters = [new Monster()]; 
         this.gracz1 = new Monster("Baba Wodna", 120, 5, 2, 23, 20);
         this.gracz2 = new Monster("Strzyga", 75, 8, 3, 25, 20);
         this.space = document.addEventListener('keypress', this.onKeyPress.bind(this));
@@ -56,6 +56,9 @@ class Game {
         this.btn.onclick = this.round.bind(this);
         this.gracz1Data = document.getElementById("gracz1Data");
         this.gracz2Data = document.getElementById("gracz2Data");
+        this.setInputValues(1, this.gracz1.hp, this.gracz1.maxDmg, this.gracz1.minDmg, this.gracz1.critChance, this.gracz1.missChance);
+        this.setInputValues(2, this.gracz2.hp, this.gracz2.maxDmg, this.gracz2.minDmg, this.gracz2.critChance, this.gracz2.missChance);
+
     }
         
     onKeyPress(e) {
@@ -65,8 +68,9 @@ class Game {
         }
     }
     round() {
-        this.gracz1.hit(this.gracz2);
-        this.gracz2.hit(this.gracz1);
+        const dmg1 = this.gracz1.hit(this.gracz2);
+        const dmg2 = this.gracz2.hit(this.gracz1);
+        document.querySelector('#shared_battle_log').innerHTML += this.newRoundTemplate(this.gracz1.name, this.gracz1.hp, dmg1, this.gracz2.name, this.gracz2.hp, dmg2);
         if (this.gracz1.isDead()) {
             if(confirm('this.gracz2.name + " win. Press OK to restart."')) {
                 const game = new Game();
@@ -80,24 +84,33 @@ class Game {
         }
         console.log(this.gracz1, this.gracz2);
     }
+    
+    newRoundTemplate(name1, hp1, dmg1, name2, hp2, dmg2) {
+        return `
+        <li class="round">
+            <div class="monster-container">
+                <h3 class="monster" id="monster1">` + name1 + `</h3>
+                <span class="hp" id=hp-monster1>` + hp1 + `</span>
+                <span class="damageDeal" id=dd-monster1>` + dmg1 + `</span>
+            </div>
+            <div class="monster-container">
+                <h3 class="monster" id="monster2">` + name2 + `</h3>
+                <span class="hp" id=hp-monster2>` + hp2 + `</span>
+                <span class="damageDeal" id=dd-monster2>` + dmg2 + `</span>
+            </div>
+        </li>
+    `
+    }
+    setInputValues(player, hp, maxDmg, minDmg, critChance, missChance) {
+        const d = document;
+        d.getElementById('hp' + player).value = hp;
+        d.getElementById('maxDamage' + player).value = maxDmg;
+        d.getElementById('minDamage' + player).value = minDmg;
+        d.getElementById('criticChance' + player).value = critChance;
+        d.getElementById('missChance' + player).value = missChance;
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 window.addEventListener('DOMContentLoaded', (e) => {
   const game = new Game();
 });
-
-
-
-
