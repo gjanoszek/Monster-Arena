0c8d3ec3c706be42e188762291358ff431e4229d
Grzesiek <grzechu.noname@gmail.com>
2020-03-18 22:36:24 +0100

    css + more monsters

--- MonsterBattle.js
+++ MonsterBattle.js
@@ -48,17 +48,34 @@ class Monster {
 
 class Game {
     constructor() {
-        // this.monsters = [new Monster()]; 
-        this.gracz1 = new Monster("Baba Wodna", 120, 5, 2, 23, 20);
-        this.gracz2 = new Monster("Strzyga", 75, 8, 3, 25, 20);
-        this.space = document.addEventListener('keypress', this.onKeyPress.bind(this));
-        this.btn = document.getElementById("beginBattle");
-        this.btn.onclick = this.round.bind(this);
-        this.gracz1Data = document.getElementById("gracz1Data");
-        this.gracz2Data = document.getElementById("gracz2Data");
+        this.monsters = [
+            new Monster("Baba Wodna", 120, 5, 2, 23, 20),
+            new Monster("Strzyga", 75, 8, 3, 25, 20),
+            new Monster("Ścierwojad", 60, 9, 2, 25, 20),
+            new Monster("Cieniostwór", 100, 6, 2, 25, 20),
+            new Monster("Zębacz", 90, 10, 1, 25, 20)
+        ]; 
+        this.gracz1 = this.monsters[0];
+        this.gracz2 = this.monsters[0];
         this.setInputValues(1, this.gracz1.hp, this.gracz1.maxDmg, this.gracz1.minDmg, this.gracz1.critChance, this.gracz1.missChance);
         this.setInputValues(2, this.gracz2.hp, this.gracz2.maxDmg, this.gracz2.minDmg, this.gracz2.critChance, this.gracz2.missChance);
 
+        this.gracz1Data = document.getElementById("gracz1Data");
+        this.gracz2Data = document.getElementById("gracz2Data");
+
+        this.gracz1Data.onchange = () => {
+            this.gracz1 = this.monsters[this.gracz1Data.options[this.gracz1Data.selectedIndex].value];
+            this.setInputValues(1, this.gracz1.hp, this.gracz1.maxDmg, this.gracz1.minDmg, this.gracz1.critChance, this.gracz1.missChance);
+        }
+
+        this.gracz2Data.onchange = () => {
+            this.gracz2 = this.monsters[this.gracz2Data.options[this.gracz2Data.selectedIndex].value];
+            this.setInputValues(2, this.gracz2.hp, this.gracz2.maxDmg, this.gracz2.minDmg, this.gracz2.critChance, this.gracz2.missChance);
+        }
+
+        this.space = document.addEventListener('keypress', this.onKeyPress.bind(this));
+        this.btn = document.getElementById("beginBattle");
+        this.btn.onclick = this.round.bind(this);
     }
         
     onKeyPress(e) {
@@ -89,14 +106,14 @@ class Game {
         return `
         <li class="round">
             <div class="monster-container">
-                <h3 class="monster" id="monster1">` + name1 + `</h3>
-                <span class="hp" id=hp-monster1>` + hp1 + `</span>
-                <span class="damageDeal" id=dd-monster1>` + dmg1 + `</span>
+                <h3 class="monster" id="monster1">${name1}</h3>
+                <span class="hp" id=hp-monster1>${hp1}</span>
+                <span class="damageDeal" id=dd-monster1>${dmg1}</span>
             </div>
             <div class="monster-container">
-                <h3 class="monster" id="monster2">` + name2 + `</h3>
-                <span class="hp" id=hp-monster2>` + hp2 + `</span>
-                <span class="damageDeal" id=dd-monster2>` + dmg2 + `</span>
+                <h3 class="monster" id="monster2">${name2}</h3>
+                <span class="hp" id=hp-monster2>${hp2}</span>
+                <span class="damageDeal" id=dd-monster2>${dmg2}</span>
             </div>
         </li>
     `
